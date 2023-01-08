3f24b995c3d5846f9cc00a6bb1c55eeff8c08bf5
John Damilola Ogungbure <jogungbure@gmail.com>
2023-01-08 15:32:16 +0100

    changes  for init deployment

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import config from '../config'
 import { authRouter } from '../components/auth/routes'
 import { walletRouter } from '../components/wallet/routes'
@@ -6,6 +6,12 @@ import { walletRouter } from '../components/wallet/routes'
 const router = express.Router()
 
 const { PREFIX } = config.api
+
+router.get('/', (req: Request, res: Response) => {
+	return res
+		.status(200)
+		.send({ msg: `${config.app.NAME} is running on port ${config.app.PORT}` })
+})
 
 router.use(`/${PREFIX}/auth`, authRouter)
 router.use(`/${PREFIX}/wallet`, walletRouter)
