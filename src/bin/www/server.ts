2ca78b5a6ab4b75d062e8d5875fb5fe4879b624f
John Damilola Ogungbure <jogungbure@gmail.com>
2023-01-09 20:59:40 +0100

    deploy fix

--- src/bin/www/server.ts
+++ src/bin/www/server.ts
@@ -1,18 +1,19 @@
-import http from 'http'
+// import http from 'http'
 import config from '../../config'
 import { Logger } from '../../library/helpers'
 import Application from '../../app'
 import ConnectDatabase from '../../db/connectDB'
 
-const PORT = config.app.PORT || 4000
 const app = Application()
 
 ConnectDatabase().catch(err => Logger.error(err))
 
-new http.Server(app).listen(PORT, () => {
+app.listen(config.app.PORT || 4000, () => {
 	Logger.info(`
   -------------------------------------------
-    ${config.app.NAME?.toUpperCase()} Server listening on port ${PORT}
+    ${config.app.NAME?.toUpperCase()} Server listening on port ${
+		config.app.PORT || 4000
+	}
   -------------------------------------------
   `)
 })
